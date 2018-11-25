4212ed484aa2ec40552ca4425ea7d5635b30572c
Matt Biddulph <mb@hackdiary.com>
2018-11-25 13:39:32 -0800

    Consolidate model classes into a single file.

--- src/Sequence.cpp
+++ src/Sequence.cpp
@@ -1,6 +1,22 @@
-#include "Envelope.hpp"
 #include "Sequence.hpp"
 #include <vector>
+#include <math.h>
 
-float Sequence::f(float t) { return envelopes.front()->f(t); }
+float Segment::f(float t) { return m * pow(t, n) + b; }
+
+Segment::Segment() {
+    m = 1;
+    n = 1.2;
+    b = 0;
+}
+
+Envelope::Envelope() {
+    segments.push_back(new Segment());
+    onStep = 0;
+    offStep = 1;
+    isQuantized = false;
+}
+float Envelope::f(float t) { return segments.front()->f(t); }
+
 Sequence::Sequence() { envelopes.push_back(new Envelope()); }
+float Sequence::f(float t) { return envelopes.front()->f(t); }
--- src/Sequence.hpp
+++ src/Sequence.hpp
@@ -1,5 +1,25 @@
-#include "Envelope.hpp"
 #include <vector>
+
+class Segment {
+    float m;
+    float b;
+    float n;
+
+public:
+    float f(float t);
+    Segment();
+};
+
+class Envelope {
+    int onStep;
+    int offStep;
+    bool isQuantized;
+    std::vector<Segment *> segments;
+
+public:
+    float f(float t);
+    Envelope();
+};
 
 class Sequence {
   std::vector<Envelope *> envelopes;
